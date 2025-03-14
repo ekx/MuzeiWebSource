6c4e891464e5ca51328d15a81460bf3e765fc483
Benjamin Dengler <benjamin.dengler@gmail.com>
2025-03-14 17:43:01 +0100

    fixed .gitignore

---
+++ app/src/main/java/com/google/android/apps/muzei/websource/WebSourceArtProvider.kt
@@ -0,0 +1,9 @@
+package com.google.android.apps.muzei.websource
+
+import com.google.android.apps.muzei.api.provider.MuzeiArtProvider
+
+class WebSourceArtProvider : MuzeiArtProvider() {
+    override fun onLoadRequested(initial: Boolean) {
+        TODO("Not yet implemented")
+    }
+}
